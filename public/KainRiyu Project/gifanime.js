edbdbe995f91e48b0ddc3dfcf3dd40e3957a433c
Kain Naji <120968486+KainLoveYuji@users.noreply.github.com>
2024-04-15 20:38:16 +0700

    Create gifanime.js

---
+++ public/KainRiyu Project/gifanime.js
@@ -0,0 +1,17 @@
+exports.name = '/gifanime';
+exports.index = async(req, res, next) => {
+    const fs = require('fs-extra');
+    try {
+        let dirPath = __dirname + `/data/txt/gifanime.txt`;
+        var KainRiyu = (fs.readFileSync(dirPath, "utf-8").split(/\r?\n/));
+        var KainProject = KainRiyu[Math.floor(Math.random() * KainRiyu.length)].trim();
+        res.jsonp({
+            url: KainProject,
+            data: KainProject,
+            count: KainRiyu.length,
+            author: "Kain Naji"
+        });
+    } catch (e) {
+        return res.jsonp({ error: e });
+    }
+}

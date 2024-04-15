554559b577ce662b52e968dac83a5e372a7694f7
Kain Naji <120968486+KainLoveYuji@users.noreply.github.com>
2024-04-15 20:41:46 +0700

    Create phongcanh.js

---
+++ public/KainRiyu Project/phongcanh.js
@@ -0,0 +1,17 @@
+exports.name = '/phongcanh2';
+exports.index = async(req, res, next) => {
+    const fs = require('fs-extra');
+    try {
+        let dirPath = __dirname + `/data/txt/phongcanh2.txt`;
+        var KainRiyu = (fs.readFileSync(dirPath, "utf-8").split(/\r?\n/));
+        var KainProject = KainRiyu[Math.floor(Math.random() * KainRiyu.length)].trim();
+        res.jsonp({
+            url: KainProject,
+            data: KainProject,
+            count: KainRiyu.length,
+            author: "Kain Naji"
+        });
+    } catch (e) {
+        return res.jsonp({ error: e });
+    }
+}

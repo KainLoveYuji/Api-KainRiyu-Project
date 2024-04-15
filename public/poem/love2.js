59eb1066a15433fbfe24f51429535c56b818adfb
Kain Naji <120968486+KainLoveYuji@users.noreply.github.com>
2024-04-15 16:53:10 +0700

    Update love2.js

--- public/poem/love2.js
+++ public/poem/love2.js
@@ -1,1 +1,15 @@
-
+exports.name = '/poem/love2';
+exports.index = async(req, res, next) => {
+    try {
+        const KainRiyu = require('./data/txt/love2.txt');
+        var image = KainRiyu[Math.floor(Math.random() * KainRiyu.length)].trim();
+        res.jsonp({
+            url: image,
+            data: image,
+            count: KainRiyu.length,
+            author: 'Kain Naji'
+        });
+    } catch (e) {
+        return res.jsonp({ error: e });
+    }
+}

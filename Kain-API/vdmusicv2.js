add73e87f4c4723ab426cb811cbdebf418fc96d7
Kain Naji <120968486+KainLoveYuji@users.noreply.github.com>
2024-04-15 17:59:18 +0700

    Update vdmusicv2.js

--- Kain-API/vdmusicv2.js
+++ Kain-API/vdmusicv2.js
@@ -1,1 +1,24 @@
+const path = require('path');
+const { readFileSync } = require('fs-extra');
 
+exports.name = '/vdmusicv2';
+exports.index = async (req, res, next) => {
+    try {
+        const filePath = path.join(__dirname, 'Kz-API', 'json', 'vdmusicv2.txt');
+        const KainRiyu = readFileSync(filePath, 'utf-8')
+            .split('\n')
+            .map(line => line.trim())
+            .filter(line => line); // Remove empty lines
+
+        var image = KainRiyu[Math.floor(Math.random() * KainRiyu.length)];
+
+        res.jsonp({
+            url: image,
+            data: image,
+            count: KainRiyu.length,
+            author: 'Kain Naji'
+        });
+    } catch (e) {
+        return res.jsonp({ error: e });
+    }
+};

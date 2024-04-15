23a4aebd69804aa697b0a1ce58fa9b94d5eef00c
Kain Naji <120968486+KainLoveYuji@users.noreply.github.com>
2024-04-15 18:29:20 +0700

    Update vdgaiv2.js

--- Kain-API/vdgaiv2.js
+++ Kain-API/vdgaiv2.js
@@ -5,10 +5,6 @@ exports.name = '/vdgaiv2';
 exports.index = async (req, res, next) => {
     try {
         const KainRiyu = require('./Kz-API/json/vdgaiv2.json');
-            .split('\n')
-            .map(line => line.trim())
-            .filter(line => line); // Remove empty lines
-
         var image = KainRiyu[Math.floor(Math.random() * KainRiyu.length)];
 
         res.jsonp({

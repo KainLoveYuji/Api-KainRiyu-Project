f580e5e920d219bce42a3ddb0ebc9f811bc541a4
Kain Naji <120968486+KainLoveYuji@users.noreply.github.com>
2024-08-21 21:51:49 +0700

    Update characterapi.js

--- public/Hoyoverse/characterapi.js
+++ public/Hoyoverse/characterapi.js
@@ -4,17 +4,13 @@ const path = require('path');
 
 exports.name = '/Hoyoverse';
 exports.index = async (req, res, next) => {
-    // Đường dẫn tới thư mục chứa thông tin nhân vật
     const charactersDir = path.join(__dirname, '/characters');
     
-    // Đọc danh sách các file JSON trong thư mục
     const characterFiles = fs.readdirSync(charactersDir);
 
-    // Tạo danh sách tên nhân vật từ các file JSON
     let characterNames = [];
     let charactersData = {};
 
-    // Đọc dữ liệu từ các file JSON
     for (const file of characterFiles) {
         const filePath = path.join(charactersDir, file);
         const data = fs.readFileSync(filePath, 'utf-8');
@@ -26,16 +22,15 @@ exports.index = async (req, res, next) => {
         });
     }
 
-    const url = req.query.key;
-    if (!url) return res.jsonp({ error: 'Thiếu dữ liệu để khởi chạy chương trình' });
+    // Lấy tham số từ phần thân của yêu cầu
+    const { name: characterName } = req.body;
+    if (!characterName) return res.jsonp({ error: 'Thiếu dữ liệu để khởi chạy chương trình' });
 
-    const checker = stringSimilarity.findBestMatch(url, characterNames);
+    const checker = stringSimilarity.findBestMatch(characterName, characterNames);
     const bestMatchName = checker.bestMatch.target;
 
-    // Tìm thông tin nhân vật từ dữ liệu đã đọc
     const character = charactersData[bestMatchName];
     if (!character) return res.jsonp({ error: 'Nhân vật không tìm thấy' });
 
     res.json({ character });
 };
-
